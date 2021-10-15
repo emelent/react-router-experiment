808c3060a42fd95d872114bb3db56fe07ca08e5d
Roy Motloutsi <roy.motloutsi@gmail.com>
2021-10-15 10:30:20 +0200

    remove dashboard

--- src/example.js
+++ src/example.js
@@ -9,15 +9,6 @@ import {
   useLocation
 } from "react-router-dom";
 
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
-
 export default function BasicExample() {
   return (
     <Router>
@@ -30,31 +21,18 @@ export default function BasicExample() {
             <Link to="/about">About</Link>
           </li>
           <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
             <Link to="/profile">Profile</Link>
           </li>
         </ul>
 
         <hr />
 
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
         <Switch>
           <Route exact path="/">
             <Home />
           </Route>
           <Route path="/about">
             <About />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
           </Route>
           <Route path="/profile">
             <ProfileContainer />
@@ -84,19 +62,10 @@ function Home() {
 function About() {
   let location = useLocation();
 
-  console.log(location);
   return (
     <div>
       <h2>About</h2>
       {location.state && `With some extra ${location.state.extra}`}
-    </div>
-  );
-}
-
-function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
     </div>
   );
 }
