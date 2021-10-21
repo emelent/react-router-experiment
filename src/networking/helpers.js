b68faa6b555879ad3b98261d65781de62f16e15b
Roy <roy.motloutsi@gmail.com>
2021-10-21 16:30:37 +0200

    extend Error

--- src/networking/helpers.js
+++ src/networking/helpers.js
@@ -12,8 +12,9 @@ export function toJsonOperator(response) {
     return response.json()
 }
 
-export class NetworkError {
-    constructor(response) {
+export class NetworkError extends Error {
+    constructor(response, message) {
+        super(message? message: `${response.status}:${response.url}`)
         this.url = response.url
         this.status = response.status
     }
